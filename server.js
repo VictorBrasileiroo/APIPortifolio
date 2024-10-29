137f6eb4ec732c301e4ad74274b50f4b8e325886
Your Name <victor.lopesvfk11@gmail.com>
2024-10-29 13:04:42 -0300

    commit vercel

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// backend/server.js
+// server.js
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -9,10 +9,17 @@ app.use(express.json());
 
 const projects = require('./projects.json');
 
+// Rota para a raiz (evita erro "Cannot GET /")
+app.get('/', (req, res) => {
+    res.send('API Backend rodando! Use /api/projects para acessar os projetos.');
+});
+
+// Rota para retornar os projetos
 app.get('/api/projects', (req, res) => {
     res.json(projects);
 });
 
+// Inicializando o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
