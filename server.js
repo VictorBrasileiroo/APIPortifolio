febd2979f0edbaf30dc9b620788084e6a85f1ca1
Your Name <victor.lopesvfk11@gmail.com>
2024-10-29 13:49:32 -0300

    commit

--- server.js
+++ server.js
@@ -9,17 +9,14 @@ app.use(express.json());
 
 const projects = require('./projects.json');
 
-// Rota para a raiz (evita erro "Cannot GET /")
 app.get('/', (req, res) => {
     res.send('API Backend rodando! Use /api/projects para acessar os projetos.');
 });
 
-// Rota para retornar os projetos
 app.get('/api/projects', (req, res) => {
     res.json(projects);
 });
 
-// Inicializando o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
